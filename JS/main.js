b1b077df0f2a9d9cb6930f4217f411d908e07fdc
Oguzhan <kayaoguzhan@windowslive.com>
2019-02-15 20:58:35 +0300

    video plays

--- JS/main.js
+++ JS/main.js
@@ -1,54 +1,49 @@
-// Global Variables
-
-const width = 500,
-      height = 0,
-      filter ="none",
-      streaming = false;
+let width = 500,
+    height = 0,
+    filter = 'none',
+    streaming = false;
 
 // DOM Elements
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const photos = document.getElementById('photos');
+const photoButton = document.getElementById('photo-button');
+const clearButton = document.getElementById('clear-button');
+const photoFilter = document.getElementById('photo-filter');
 
-const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
-const photos = document.getElementById("photos");
-const photoButton = document.getElementById("photo-button");
-const clearButton = document.getElementById("clear-button");
-const photoFilter = document.getElementById("photo-filter");
+// Get media stream
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(function(stream) {
+        // Link to the video source
+        video.srcObject = stream;
+        // Play video
+        video.play();
+    })
+    .catch(function(err) {
+        console.log(`Error: ${err}`);
+    });
 
-//Get media Stream
+// Play when ready
+video.addEventListener('canplay', function(e) {
+    if(!streaming) {
+        // Set video / canvas height
+        height = video.videoHeight / (video.videoWidth / width);
 
-navigator.mediaDevices.getUserMedia({video:true, audio: false})
-.then((stream) => {
- //link to the video source
- video.srcObject = stream;
- //play video
-    video.play();
-})
-.catch((err) => {
-    console.log("Error",err)
-});
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
 
-//  Play when ready
-
-video.addEventListener("canplay", function (e) {
-
-    if(!streaming){
-        //set video / canvas height
-        let height = video.videoHeight / (video.videoWidth/ width);
-        video.setAttribute("width", width);
-        video.setAttribute("height", height);
-        canvas.setAttribute("width", width);
-        canvas.setAttribute("height", height);
-
-       let streaming = true;
+        streaming = true;
     }
 }, false);
 
-photoButton.addEventListener("click", function (e) {
+// Photo button event
+photoButton.addEventListener('click', function(e) {
     takePicture();
 
     e.preventDefault();
 }, false);
-
 function takePicture() {
     //Create Canvas
     const context = canvas.getContext("2d");
